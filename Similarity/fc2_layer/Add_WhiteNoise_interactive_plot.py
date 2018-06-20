c2830c57e3742c1c51f5aaa754d9ec17e6b308c6
Oneding <meridianz777@hotmail.com>
2018-06-20 18:31:01 -0400

    Update Add_WhiteNoise_interactive_plot.py

--- Similarity/fc2_layer/Add_WhiteNoise_interactive_plot.py
+++ Similarity/fc2_layer/Add_WhiteNoise_interactive_plot.py
@@ -24,8 +24,19 @@ from keras.applications.inception_v3 import InceptionV3
 from keras.applications.resnet50 import ResNet50
 todate = datetime.now().strftime('%Y%m%d')
 
+'''
+For the following code, we want to add different scales of white noises (transformation)
+to our motabolite image and then test their similarity levels. In the meantime, we process
+the metabolite image through a transfer learning model before appling the same similarity test.
+
+We want to test if the transfer learning model would do a better job at recognizing the motablite 
+image even after transformations. In other words, we are interested to learn what transfer learning 
+really do to our motabolite image. We use matrix norm of the image as a scaler for similarities.
+'''
+
+# ----------------------------------------------- data clean & transform -----------------------------------------------
 def load_data(i_img):
-    data_dir = '/'
+    data_dir = '/directory/to/image/file'
     os.chdir(data_dir)
     img1 = np.loadtxt('./Results-NP_C3_N/Images.txt')[i_img]
     return img1
@@ -33,6 +44,7 @@ def load_data(i_img):
 def data_preprocess(img):
     return img if max(img)==0 else img/max(img)*1.0
 
+# image transformation
 def white_noise(img, img_col, img_row, intensity):
     img_tmp=[]
     for i in range(len(img)):
@@ -40,6 +52,8 @@ def white_noise(img, img_col, img_row, intensity):
     img_tmp = np.asarray(img_tmp)
     return img_tmp
 
+# ----------------------------------------------- transfer learning -----------------------------------------------
+# resize images to fit the tranfer-learning model (VGG16: 1 channel gray-scale(60, 12, 1) to 3 channels RGB(224, 224, 3))
 def image_transform(img, img_col, img_row):
     img_resize = cv2.resize(img.reshape((img_col, img_row)), (224, 224), interpolation = cv2.INTER_LINEAR)
     img_add_channel = Image.fromarray(np.uint8(cm.jet(img_resize)*255)).convert("RGB")
@@ -55,6 +69,8 @@ def choose_model(which_model):
         model = InceptionV3(input_tensor=image_input, weights='imagenet', include_top=True)
     elif which_model == 'resnet50':
         model = ResNet50(input_tensor=image_input, weights='imagenet', include_top=True)
+
+    # stop transfer learning at 'fc2' layer (with 4096 parameters) before the final 'prediction' layer
     base_pred = model.get_layer('fc2').output
     base_model = Model(image_input, base_pred)
     # print(base_model.summary())
@@ -71,15 +87,16 @@ def generate4096(img1_trans, img1_sft_trans):
     print('training time: {0:.4f} seconds'.format(time.time() - start_timing))
     return img1_4096, img1_sft_4096
 
-# ----------------------------------------------------------------------------
-
+# ----------------------------------------------- main -----------------------------------------------
 def main_vgg(i_img=27, intensity=20, save=False):
     img1 = load_data(i_img)
     img1 = data_preprocess(img1)
     img_col, img_row = 60, 12
+    
     # shift left pixels
     img1_sft = white_noise(img1, img_col, img_row, intensity)
     print('shift {}px left in img1'.format(intensity))
+    
     # plot image
     plt.subplot(121)
     plt.imshow(img1.reshape((img_col, img_row)), cmap='jet')
@@ -88,30 +105,28 @@ def main_vgg(i_img=27, intensity=20, save=False):
     plt.imshow(img1_sft.reshape((img_col, img_row)), cmap='jet')
     plt.title('img1_white_noise')
     # plt.show()
+    
     # matrix norm
     img1_mn = img1 / np.linalg.norm(img1)
     img1_sft_mn = img1_sft / np.linalg.norm(img1_sft)
     simi_720 = np.dot(img1_mn, img1_sft_mn)
+    
     # plot images after resize to fit vgg16
     img1_trans = image_transform(img1, img_col, img_row)
     img1_sft_trans = image_transform(img1_sft, img_col, img_row)
     vgg_img_col, vgg_img_row = 224, 224
-    # plt.subplot(121)
-    # plt.imshow(img1_trans, cmap='jet')
-    # plt.title('img1')
-    # plt.subplot(122)
-    # plt.imshow(img1_sft_trans, cmap='jet')
-    # plt.title('img1_sft')
-    # plt.show()
+    
     # add one dimension
     img1_trans = np.expand_dims(img1_trans, axis=0)
     img1_sft_trans = np.expand_dims(img1_sft_trans, axis=0)
     # print(img1_trans.shape)
     # print(img1_sft_trans.shape)
+    
     # generate vgg-ready image
     img1_4096, img1_sft_4096 = generate4096(img1_trans, img1_sft_trans)
     img1_4096 = np.squeeze(img1_4096, axis=0)
     img1_sft_4096 = np.squeeze(img1_sft_4096, axis=0)
+    
     # matrix norm
     img1_4096_mn = img1_4096 / np.linalg.norm(img1_4096)
     img1_sft_4096_mn = img1_sft_4096 / np.linalg.norm(img1_sft_4096)
@@ -120,6 +135,7 @@ def main_vgg(i_img=27, intensity=20, save=False):
     # print('similarity level w/ vgg: {0:.4f}'.format(simi_4096))
     return simi_720, simi_4096
 
+# loop throgh different scale/percentage of transformation
 def simi_loop(i_img=27, intensity=20, save=False):
     simi_720_lst, simi_4096_lst = [], []
     for i in range(intensity):
@@ -139,5 +155,6 @@ def simi_loop(i_img=27, intensity=20, save=False):
     plt.show()
     return simi_720_lst, simi_4096_lst
 
+# ----------------------------------------------- main -----------------------------------------------
 # i_img = image index; num_sft = maximum shifts allowed
 simi_720_lst_27, simi_4096_lst_27 = simi_loop(i_img=4, intensity=20)
